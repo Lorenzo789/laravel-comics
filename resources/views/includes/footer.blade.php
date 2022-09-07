aefa7f5976ee419600d4e86a0549e7002552576a
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-07 15:51:21 +0200

    start

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <section class="container">
+        <button>Sign Up Now</button>
+        <nav>
+            <span>Follow Us</span>
+            <a href="#"><img src="" alt=""></a>
+            <a href="#"><img src="" alt=""></a>
+            <a href="#"><img src="" alt=""></a>
+            <a href="#"><img src="" alt=""></a>
+            <a href="#"><img src="" alt=""></a>
+        </nav>
+    </section>
+</footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <img src="{{url('/assets/dc-logo.png')}}" alt="">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
